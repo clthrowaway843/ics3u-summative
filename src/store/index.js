56f8fbba1faaaedb97cab3045b30a9a54c3ae602
sweetbambie <sophia@miao.fm>
2025-01-18 04:09:52 +0000

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,7 @@ import { auth } from "../firebase";
 export const useStore = defineStore('store', () => {
   const user = ref(null);
   const cart = ref(new Map());
+  const checkoutCompleted = ref(false);
 
   function addToCart(id, movieData) {
     cart.value.set(id, movieData);
@@ -19,8 +20,23 @@ export const useStore = defineStore('store', () => {
   }
 
   function clearCart() {
-    cart.value.clear(); 
-    saveCartToLocalStorage(); 
+    cart.value.clear();  
+    saveCartToLocalStorage();
+  
+
+    console.log("Checkout started:", checkoutCompleted.value);
+  
+    checkoutCompleted.value = true;
+  
+
+    console.log("Checkout completed:", checkoutCompleted.value);
+  
+    setTimeout(() => {
+      checkoutCompleted.value = false;
+  
+
+      console.log("Checkout reset:", checkoutCompleted.value);
+    }, 3000);  
   }  
 
   function saveCartToLocalStorage() {
